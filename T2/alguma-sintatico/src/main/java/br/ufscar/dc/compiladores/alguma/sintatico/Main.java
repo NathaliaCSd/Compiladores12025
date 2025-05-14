cc817a95b83a43bba4863d280127a5a241df41f5
Guilherme Castro <guilherme_lol75@hotmail.com>
2025-05-14 16:36:13 -0300

    casos t2 atualizados

--- T2/alguma-sintatico/src/main/java/br/ufscar/dc/compiladores/alguma/sintatico/Main.java
+++ T2/alguma-sintatico/src/main/java/br/ufscar/dc/compiladores/alguma/sintatico/Main.java
@@ -3,6 +3,8 @@ package br.ufscar.dc.compiladores.alguma.sintatico;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CharStream;
@@ -19,29 +21,61 @@ public class Main {
             return;
         }
 
-        // Leitura do arquivo de entrada
         CharStream cs = CharStreams.fromFileName(args[0]);
         AlgumaLexer lexer = new AlgumaLexer(cs);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-        try (PrintWriter pw = new PrintWriter(new FileWriter(args[1]))) {
+        List<String> saidaTokens = new ArrayList<>();
+        boolean erroLexico = false;
+        String erroLexicoMsg = "";
 
-            // 1. Imprime os tokens reconhecidos
-            Token t = null;
-            while ((t = lexer.nextToken()).getType() != Token.EOF) {
-                String nomeToken = AlgumaLexer.VOCABULARY.getDisplayName(t.getType());
-                String texto = t.getText();
-                //pw.println("<'" + texto + "'," + nomeToken + ">");
+        // 1. Realiza análise léxica e armazena tokens e possíveis erros
+        Token t;
+        while ((t = lexer.nextToken()).getType() != Token.EOF) {
+            String nomeToken = AlgumaLexer.VOCABULARY.getDisplayName(t.getType());
+            String texto = t.getText();
+            /*erroLexicoMsg = "Linha " + t.getLine() + ": cadeia literal nao fechada";
+            erroLexico = true;*/
+            if ("CADEIA_INACABADA".equals(nomeToken)) {
+                erroLexicoMsg = "Linha " + t.getLine() + ": cadeia literal nao fechada\nFim da compilacao";
+                
+                erroLexico = true;
+                break;
             }
 
-            // 2. Reinicializa o CharStream e o lexer, porque ele já foi consumido
+            if ("COMENTARIO_INACABADO".equals(nomeToken)) {
+                erroLexicoMsg = "Linha " + t.getLine() + ": comentario nao fechado\nFim da compilacao";
+                erroLexico = true;
+                break;
+            }
+
+            if ("ERROR".equals(nomeToken)) {
+                erroLexicoMsg = "Linha " + t.getLine() + ": " + texto + " - simbolo nao identificado\nFim da compilacao";
+                erroLexico = true;
+                break;
+            }
+
+            if (!nomeToken.equals("WS") && !nomeToken.equals("COMENTARIO")) {
+                saidaTokens.add("<'" + texto + "'," + nomeToken + ">");
+            }
+        }
+
+        // 2. Se houver erro léxico, imprime só ele
+        try (PrintWriter pw = new PrintWriter(new FileWriter(args[1]))) {
+            if (erroLexico) {
+                pw.println(erroLexicoMsg);
+                return;
+            }
+
+            // 3. Reinicializa lexer/parser
             cs = CharStreams.fromFileName(args[0]);
             lexer = new AlgumaLexer(cs);
-            tokens = new CommonTokenStream(lexer);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
             AlgumaParser parser = new AlgumaParser(tokens);
 
-            // 3. Remove o listener padrão e adiciona um personalizado
+            // 4. Adiciona listener de erro sintático
             parser.removeErrorListeners();
+            final boolean[] erroSintatico = {false};
+
             parser.addErrorListener(new BaseErrorListener() {
                 @Override
                 public void syntaxError(Recognizer<?, ?> recognizer,
@@ -50,15 +84,30 @@ public class Main {
                                         int charPositionInLine,
                                         String msg,
                                         RecognitionException e) {
-                    pw.println("Linha " + line + ": erro sintatico proximo a " + ((Token) offendingSymbol).getText());
+                    erroSintatico[0] = true;
+                    String textoErro = ((Token) offendingSymbol).getText();
+                                            if ("<EOF>".equals(textoErro)) {
+                                                textoErro = "EOF";
+                                            }
+                                            pw.println("Linha " + line + ": erro sintatico proximo a " + textoErro);
+                    pw.println("Fim da compilacao");
+                    // Interrompe imediatamente com uma exceção
+                    throw new RuntimeException("Erro sintático encontrado");
                 }
             });
 
-            // 4. Executa o parser
+            // 5. Executa o parser
             parser.programa();
-            pw.println("Fim da compilacao");
+
+            // 6. Se não houve erro sintático, imprime os tokens
+            if (!erroSintatico[0]) {
+                for (String s : saidaTokens) {
+                    pw.println(s);
+                }
+            }
+
         } catch (IOException e) {
-            System.err.println("Erro ao escrever no arquivo de saída: " + args[1]);
+            System.err.println("Erro ao escrever no arquivo de saída.");
         }
     }
 }

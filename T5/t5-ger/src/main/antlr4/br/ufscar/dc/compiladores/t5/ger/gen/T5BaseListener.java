cb01ad3e9f52e127fe45bbefdbd69790d5354623
Guilherme Castro <guilherme_lol75@hotmail.com>
2025-07-02 06:20:18 -0300

    base do t6

---
+++ T5/t5-ger/src/main/antlr4/br/ufscar/dc/compiladores/t5/ger/gen/T5BaseListener.java
@@ -0,0 +1,626 @@
+// Generated from c:\UFSCar\Compiladores\T1\Compiladores12025\T5\t5-ger\src\main\antlr4\br\u005Cufscar\dc\compiladores\t5\ger\T5.g4 by ANTLR 4.10.1
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link T5Listener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class T5BaseListener implements T5Listener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterPrograma(T5Parser.ProgramaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitPrograma(T5Parser.ProgramaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDeclaracoes(T5Parser.DeclaracoesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDeclaracoes(T5Parser.DeclaracoesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDeclaracao_local(T5Parser.Declaracao_localContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDeclaracao_local(T5Parser.Declaracao_localContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVariavel(T5Parser.VariavelContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVariavel(T5Parser.VariavelContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIdentificador(T5Parser.IdentificadorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIdentificador(T5Parser.IdentificadorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDimensao(T5Parser.DimensaoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDimensao(T5Parser.DimensaoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTipo(T5Parser.TipoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTipo(T5Parser.TipoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterRegistro(T5Parser.RegistroContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitRegistro(T5Parser.RegistroContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTipo_estendido(T5Parser.Tipo_estendidoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTipo_estendido(T5Parser.Tipo_estendidoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTipo_basico_ident(T5Parser.Tipo_basico_identContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTipo_basico_ident(T5Parser.Tipo_basico_identContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTipo_basico(T5Parser.Tipo_basicoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTipo_basico(T5Parser.Tipo_basicoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterValor_constante(T5Parser.Valor_constanteContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitValor_constante(T5Parser.Valor_constanteContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDeclaracao_global(T5Parser.Declaracao_globalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDeclaracao_global(T5Parser.Declaracao_globalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParametros(T5Parser.ParametrosContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParametros(T5Parser.ParametrosContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParametro(T5Parser.ParametroContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParametro(T5Parser.ParametroContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCorpo(T5Parser.CorpoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCorpo(T5Parser.CorpoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCmd(T5Parser.CmdContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCmd(T5Parser.CmdContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCmdLeia(T5Parser.CmdLeiaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCmdLeia(T5Parser.CmdLeiaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCmdEscreva(T5Parser.CmdEscrevaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCmdEscreva(T5Parser.CmdEscrevaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCmdSe(T5Parser.CmdSeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCmdSe(T5Parser.CmdSeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCmdCaso(T5Parser.CmdCasoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCmdCaso(T5Parser.CmdCasoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCmdPara(T5Parser.CmdParaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCmdPara(T5Parser.CmdParaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCmdEnquanto(T5Parser.CmdEnquantoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCmdEnquanto(T5Parser.CmdEnquantoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCmdFaca(T5Parser.CmdFacaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCmdFaca(T5Parser.CmdFacaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCmdAtribuicao(T5Parser.CmdAtribuicaoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCmdAtribuicao(T5Parser.CmdAtribuicaoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCmdChamada(T5Parser.CmdChamadaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCmdChamada(T5Parser.CmdChamadaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCmdRetorne(T5Parser.CmdRetorneContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCmdRetorne(T5Parser.CmdRetorneContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSelecao(T5Parser.SelecaoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSelecao(T5Parser.SelecaoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterItem_selecao(T5Parser.Item_selecaoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitItem_selecao(T5Parser.Item_selecaoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterConstantes(T5Parser.ConstantesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitConstantes(T5Parser.ConstantesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterNumero_intervalo(T5Parser.Numero_intervaloContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitNumero_intervalo(T5Parser.Numero_intervaloContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOp_unario(T5Parser.Op_unarioContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOp_unario(T5Parser.Op_unarioContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExp_aritmetica(T5Parser.Exp_aritmeticaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExp_aritmetica(T5Parser.Exp_aritmeticaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTermo(T5Parser.TermoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTermo(T5Parser.TermoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFator(T5Parser.FatorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFator(T5Parser.FatorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOp1(T5Parser.Op1Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOp1(T5Parser.Op1Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOp2(T5Parser.Op2Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOp2(T5Parser.Op2Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOp3(T5Parser.Op3Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOp3(T5Parser.Op3Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParcela(T5Parser.ParcelaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParcela(T5Parser.ParcelaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParcela_unario(T5Parser.Parcela_unarioContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParcela_unario(T5Parser.Parcela_unarioContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParcela_nao_unario(T5Parser.Parcela_nao_unarioContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParcela_nao_unario(T5Parser.Parcela_nao_unarioContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExp_relacional(T5Parser.Exp_relacionalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExp_relacional(T5Parser.Exp_relacionalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOp_relacional(T5Parser.Op_relacionalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOp_relacional(T5Parser.Op_relacionalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpressao(T5Parser.ExpressaoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpressao(T5Parser.ExpressaoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTermo_logico(T5Parser.Termo_logicoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTermo_logico(T5Parser.Termo_logicoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFator_logico(T5Parser.Fator_logicoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFator_logico(T5Parser.Fator_logicoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParcela_logica(T5Parser.Parcela_logicaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParcela_logica(T5Parser.Parcela_logicaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOp_logico_1(T5Parser.Op_logico_1Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOp_logico_1(T5Parser.Op_logico_1Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOp_logico_2(T5Parser.Op_logico_2Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOp_logico_2(T5Parser.Op_logico_2Context ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ T5/t5-ger/src/main/antlr4/br/ufscar/dc/compiladores/t5/ger/gen/T5Visitor.java
@@ -0,0 +1,306 @@
+// Generated from c:\UFSCar\Compiladores\T1\Compiladores12025\T5\t5-ger\src\main\antlr4\br\u005Cufscar\dc\compiladores\t5\ger\T5.g4 by ANTLR 4.10.1
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link T5Parser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface T5Visitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#programa}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPrograma(T5Parser.ProgramaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#declaracoes}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDeclaracoes(T5Parser.DeclaracoesContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#declaracao_local}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDeclaracao_local(T5Parser.Declaracao_localContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#variavel}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitVariavel(T5Parser.VariavelContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#identificador}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIdentificador(T5Parser.IdentificadorContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#dimensao}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDimensao(T5Parser.DimensaoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#tipo}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTipo(T5Parser.TipoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#registro}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitRegistro(T5Parser.RegistroContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#tipo_estendido}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTipo_estendido(T5Parser.Tipo_estendidoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#tipo_basico_ident}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTipo_basico_ident(T5Parser.Tipo_basico_identContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#tipo_basico}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTipo_basico(T5Parser.Tipo_basicoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#valor_constante}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitValor_constante(T5Parser.Valor_constanteContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#declaracao_global}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDeclaracao_global(T5Parser.Declaracao_globalContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#parametros}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParametros(T5Parser.ParametrosContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#parametro}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParametro(T5Parser.ParametroContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#corpo}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCorpo(T5Parser.CorpoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#cmd}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmd(T5Parser.CmdContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#cmdLeia}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdLeia(T5Parser.CmdLeiaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#cmdEscreva}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdEscreva(T5Parser.CmdEscrevaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#cmdSe}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdSe(T5Parser.CmdSeContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#cmdCaso}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdCaso(T5Parser.CmdCasoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#cmdPara}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdPara(T5Parser.CmdParaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#cmdEnquanto}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdEnquanto(T5Parser.CmdEnquantoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#cmdFaca}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdFaca(T5Parser.CmdFacaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#cmdAtribuicao}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdAtribuicao(T5Parser.CmdAtribuicaoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#cmdChamada}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdChamada(T5Parser.CmdChamadaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#cmdRetorne}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCmdRetorne(T5Parser.CmdRetorneContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#selecao}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSelecao(T5Parser.SelecaoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#item_selecao}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitItem_selecao(T5Parser.Item_selecaoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#constantes}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitConstantes(T5Parser.ConstantesContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#numero_intervalo}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitNumero_intervalo(T5Parser.Numero_intervaloContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#op_unario}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp_unario(T5Parser.Op_unarioContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#exp_aritmetica}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExp_aritmetica(T5Parser.Exp_aritmeticaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#termo}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTermo(T5Parser.TermoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#fator}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFator(T5Parser.FatorContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#op1}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp1(T5Parser.Op1Context ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#op2}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp2(T5Parser.Op2Context ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#op3}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp3(T5Parser.Op3Context ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#parcela}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParcela(T5Parser.ParcelaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#parcela_unario}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParcela_unario(T5Parser.Parcela_unarioContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#parcela_nao_unario}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParcela_nao_unario(T5Parser.Parcela_nao_unarioContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#exp_relacional}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExp_relacional(T5Parser.Exp_relacionalContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#op_relacional}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp_relacional(T5Parser.Op_relacionalContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#expressao}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExpressao(T5Parser.ExpressaoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#termo_logico}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTermo_logico(T5Parser.Termo_logicoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#fator_logico}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFator_logico(T5Parser.Fator_logicoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#parcela_logica}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParcela_logica(T5Parser.Parcela_logicaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#op_logico_1}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp_logico_1(T5Parser.Op_logico_1Context ctx);
+	/**
+	 * Visit a parse tree produced by {@link T5Parser#op_logico_2}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOp_logico_2(T5Parser.Op_logico_2Context ctx);
+}
